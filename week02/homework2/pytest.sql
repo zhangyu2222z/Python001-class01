770ddd75a110d40604543b3bf814ad37176b3cf4
zhangy <zhangyu2222z@sina.cn>
2020-07-05 17:50:11 +0800

    数据库脚本

---
+++ week02/homework2/pytest.sql
@@ -0,0 +1,36 @@
+/*
+SQLyog Ultimate v12.08 (64 bit)
+MySQL - 8.0.11 : Database - pytest
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`pytest` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
+
+USE `pytest`;
+
+/*Table structure for table `t_movie_info` */
+
+DROP TABLE IF EXISTS `t_movie_info`;
+
+CREATE TABLE `t_movie_info` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `movie_title` varchar(64) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `movie_datetime` varchar(32) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `movie_type_name` varchar(64) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+/*Data for the table `t_movie_info` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
